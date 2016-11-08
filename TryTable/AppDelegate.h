a3bc392f38293f054f0ff9560ad9cac26dde33bb
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2016-11-07 18:50:29 -0600

    Initial Commit

---
+++ TryTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryTable
+//
+//  Created by Ziwen Chen on 11/7/16.
+//  Copyright © 2016 Ziwen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryTable
+//
+//  Created by Ziwen Chen on 11/7/16.
+//  Copyright © 2016 Ziwen Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
